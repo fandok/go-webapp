815d9af3113708fe7144af1860db184fbfd9583d
fandok <fandika.okdiba@gmail.com>
2018-01-21 23:52:04 +0700

    update test and function

--- main_test.go
+++ main_test.go
@@ -88,3 +88,26 @@ func TestRouterForNonExistentRoute(t *testing.T) {
 	}
 
 }
+
+func TestStaticFileServer(t *testing.T) {
+	r := newRouter()
+	mockServer := httptest.NewServer(r)
+
+	resp, err := http.Get(mockServer.URL + "/assets/")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// We want our status to be 200 (ok)
+	if resp.StatusCode != http.StatusOK {
+		t.Errorf("Status should be 200, got %d", resp.StatusCode)
+	}
+
+	contentType := resp.Header.Get("Content-Type")
+	expectedContentType := "text/html; charset=utf-8"
+
+	if expectedContentType != contentType {
+		t.Errorf("Wrong content type, expected %s, got %s", expectedContentType, contentType)
+	}
+
+}
