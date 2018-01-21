007022eb71af224b870d1c59fde907ea6235ff89
fandok <fandika.okdiba@gmail.com>
2018-01-21 22:54:04 +0700

    create new project

---
+++ main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+func main() {
+	r := mux.NewRouter()
+
+	r.HandleFunc("/hello", handler).Methods("GET")
+
+	http.ListenAndServe(":8080", r)
+}
+
+// "handler" is our handler function. It has to follow the function signature of a ResponseWriter and Request type
+// as the arguments.
+func handler(w http.ResponseWriter, r *http.Request) {
+	// For this case, we will always pipe "Hello World" into the response writer
+	fmt.Fprintf(w, "Hello Word!")
+}
